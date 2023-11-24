0b6702ddc6e051b216c6abcdd345d9e36e23bdc3
haron murumba <haron.murumba22@gmail.com>
2023-11-24 04:33:56 -0800

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # A puppet manifest installing flask v2.1.0 which is a package from pip3.
-# Am also specifying the version of flask to install using the ensure attribute
 
-package { 'python3-pip':
-  ensure => present,
-}
-exec { 'install flask':
-  command => 'pip3 install flask==2.1.0',
-  path => '/usr/bin',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
