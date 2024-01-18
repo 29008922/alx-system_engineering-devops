2c01d9bc1368327172f1436410140b6b4543e302
29008922 <haron.murumba22@gmail.com>
2024-01-18 12:35:12 +0000

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,12 @@ from sys import argv
 
 def number_of_subscribers(subreddit):
     '''
-        returns the number of subscribers for a given subreddit
+        returns the number of subscribers
+          for a given subreddit
     '''
     user = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
+           .format(subreddit), headers=user).json()
     try:
         return url.get('data').get('subscribers')
     except Exception:
